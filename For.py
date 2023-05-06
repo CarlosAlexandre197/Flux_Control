5a58ae79e08b532e06ba4c3f0fd1ff0405143ca5
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-05-06 14:58:15 -0300

    Atualizar o For.py

--- For.py
+++ For.py
@@ -1,6 +1,6 @@
-'''No exemplo iremos percorrer uma lista de chamada de uma determinada escola e contar quantos alunos estão presentes:'''
+'''No exemplo iremos percorrer uma lista de chamada de uma determinada escola e contar quantos alunos estão presentes na aula:'''
 
-alunos = ['ester', 'Alexandre', 'ALICE', 'Timoteo', 'Helena', 'tiago', 'SOLANGE']
+alunos = ['+55 61 8454-6713 'Alexandre', 'ALICE', 'Timoteo', 'Helena', 'tiago', 'SOLANGE']
 
 alunos_presentes = 0
 
